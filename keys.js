3605dd80c832129b07366890dd3ef90eff276446
Koni Turock <koniturock@Konis-MacBook-Air.local>
2017-04-12 17:24:53 -0400

    updated

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<J9wjA3n9EO4TqETOLmQHqgKhw>',
+  consumer_secret: '<nNFaCEg5cpFDnR7UHxwHclhdtfBAKFFPclsR0Q9DXivuYy6wzP>',
+  access_token_key: '<718266419406876672-ICLV2TwDchGSroMKDLOiBvPze3SeFig>',
+  access_token_secret: '<k5vigVdLnB5xuu31VJkycXwaozIhlaCVMnz960tBpJcmH>',
+}
